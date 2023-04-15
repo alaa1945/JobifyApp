7336c4008b9bfabcb31d48841bfb46935261847f
Alaa A.E <80849327+alaa1945@users.noreply.github.com>
2023-04-15 16:15:18 +0300

    first

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
+
+  const validate = () => {
+    const errors = {};
+
+    if (!username) {
+      errors.username = "This field can't be empty";
+    }
+
+    if (!password) {
+      errors.password = "This field can't be empty";
+    } else if (password.length < 8) {
+      errors.password = "Password must be at least 8 characters long";
+    }
+    return errors;
+  };
+  const handleSubmit = (event) => {
+    // handle login logic
+    event.preventDefault();
+    const errors = validate();
+
+    if (Object.keys(errors).length === 0) {
+      // submit form data to server
+    } else {
+      setErrors(errors);
+    }
+
+  };
   return (
     <div class="login_container">
-      <section class="h-100 gradient-form">
-        <div class="container py-5 h-100">
+      <section class="gradient-form">
+        <div class="container py-5">
           <button class="go_backBtn">
             <Link to="/" class="class_Link">
               <img
@@ -28,59 +60,124 @@ function Login() {
                           class="login-logo"
                         />
                       </div>
-
-                      <form>
-                        <div class="form-outline mb-4">
-                          <label class="form-label" for="form2Example11">
-                            Username
-                          </label>
-                          <input
-                            type="email"
-                            id="form2Example11"
-                            class="form-control"
+                      <div className="signup_methods">
+                        <div className="google-btn common_signupBtn">
+                          <div className="google-icon-wrapper">
+                            <img
+                              className="google-icon"
+                              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                              alt="google icon"
+                            />
+                          </div>
+                          <p className="btn-text">Login with google</p>
+                        </div>
+                        <div className="linkedin-btn common_signupBtn">
+                          <div className="google-icon-wrapper">
+                            <img
+                              className="google-icon"
+                              src="https://www.svgrepo.com/show/475661/linkedin-color.svg"
+                              loading="lazy"
+                              alt="google icon"
+                            />
+                          </div>
+                          <p className="btn-text">Login with LinkedIn</p>
+                        </div>
+                      </div>
+                      <div class="separator">
+                        <div class="line"></div>
+                        <div class="or">or</div>
+                        <div class="line"></div>
+                      </div>
+                      <Form onSubmit={handleSubmit} className="mt-4">
+                        <Form.Group controlId="username" className="mb-4">
+                          <Form.Label>Username</Form.Label>
+                          <Form.Control
+                            type="text"
                             placeholder="Username or email address"
+                            value={username}
+                            onChange={(event) =>
+                              setUsername(event.target.value)
+                            }
+                            isInvalid={!!errors.username}
+                            
                           />
-                        </div>
+                          <Form.Control.Feedback type="invalid">
+                            {errors.username}
+                          </Form.Control.Feedback>
+                        </Form.Group>
 
-                        <div class="form-outline mb-4">
-                          <label class="form-label" for="form2Example22">
-                            Password
-                          </label>
-                          <input
-                            type="password"
-                            id=""
-                            class="form-control"
-                            placeholder="Enter your password"
-                          />
-                        </div>
-                        <div class="buttons_div text-center pt-1 mb-5 pb-1">
-                          <button
-                            class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
-                            type="button"
+                        <Form.Group className="mb-4" controlId="password">
+                          <Form.Label>Password:</Form.Label>
+                          <div className="input-group password_group">
+                            <Form.Control
+                              type={showPassword ? "text" : "password"}
+                              value={password}
+                              placeholder="Enter you password"
+                              className="password_input"
+                              onChange={(event) =>
+                                setPassword(event.target.value)
+                              }
+                              style={{
+                                borderRight: errors.password
+                                  ? "1px solid red"
+                                  : "none",
+                                borderRadius: errors.password ? "5px" : "0px",
+                              }}
+                              isInvalid={!!errors.password}
+                            />
+                           
+                              <Button
+                                className="eye_btn"
+                                variant="outline-secondary"
+                                style={{
+                                  color: "black",
+                                  boxShadow: "none",
+                                  backgroundColor: "transparent",
+                                }}
+                                onClick={() => setShowPassword(!showPassword)}
+                              >
+                                {showPassword ? (
+                                  <i className="bi bi-eye-slash"></i>
+                                ) : (
+                                  <i className="bi bi-eye"></i>
+                                )}
+                              </Button>
+                          
+
+                            <Form.Control.Feedback type="invalid">
+                              {errors.password}
+                            </Form.Control.Feedback>
+                          </div>
+                        </Form.Group>
+
+                        <div className="buttons_div text-center pt-1 mb-5 pb-1">
+                          <Button
+                            className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
+                            type="submit"
                           >
                             Log in
-                          </button>
+                          </Button>
                           <Link
-                            class="text-muted class_Link"
+                            className="text-muted class_Link"
                             to="resetPassword"
                           >
                             Forgot password?
                           </Link>
                         </div>
 
-                        <div class="d-flex align-items-center justify-content-center pb-4">
-                          <p class="mb-0 me-2">Don't have an account?</p>
-                          <Link class="signup_link class_Link" to="/signup">
+                        <div className="login_link d-flex align-items-center justify-content-center pb-4">
+                          <p className="mb-0 me-2">Don't have an account?</p>
+                          <Link className="class_Link" to="/signup">
                             Sign Up
                           </Link>
                         </div>
-                      </form>
+                      </Form>
                     </div>
                   </div>
-                  <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                    <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                      <h4 class="mb-4">We are more than just a company</h4>
-                      <p class="small mb-0">
+                  <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
+                    <div className="text-white px-3 py-4 p-md-5 mx-md-4">
+                      <h4 className="mb-4">We are more than just a company</h4>
+                      <p className="small mb-0">
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit, sed do eiusmod tempor incididunt ut labore et
                         dolore magna aliqua. Ut enim ad minim veniam, quis
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Form, Button, Col, Row } from "react-bootstrap";
 
-
 function Signup() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -27,33 +26,26 @@ function Signup() {
 
   const validate = () => {
     const errors = {};
-
     if (!firstName) {
       errors.firstName = "This field can't be empty";
     }
-
     if (!lastName) {
       errors.lastName = "This field can't be empty";
     }
-
     if (!email) {
       errors.email = "This field can't be empty";
     }
-
     if (!password) {
       errors.password = "This field can't be empty";
     } else if (password.length < 8) {
       errors.password = "Password must be at least 8 characters long";
     }
-
     if (!country) {
       errors.country = "Please select your country";
     }
-
     if (!termsAgreed) {
       errors.termsAgreed = "You must agree to the terms of service";
     }
-
     return errors;
   };
 
@@ -68,12 +60,10 @@ function Signup() {
     }
   };
 
- 
-
   return (
-    <div className="container signup-form">
-      <section class="h-100 gradient-form">
-        <div class="container py-5 h-100">
+    <div className="container signup_container">
+      <section class="gradient-form">
+        <div class="container py-5">
           <button class="go_backBtn">
             <Link to="/" class="class_Link">
               <img
@@ -88,7 +78,7 @@ function Signup() {
               <div class="card rounded-3 text-black">
                 <div class="row g-0">
                   <div class="col-lg-12">
-                    <div class="card-body p-md-3 mx-md-4">
+                    <div class="card-body px-md-3 py-md-5 mx-md-4">
                       <div class="text-center">
                         <img
                           src={require(`../assets/images/Logo2.png`)}
@@ -96,21 +86,43 @@ function Signup() {
                           class="login-logo"
                         />
                       </div>
-                      <h2>Sign up to find work you love</h2>
                       <Form onSubmit={handleSubmit}>
-                        
+                        <div className="signup_methods">
+                          <div className="google-btn common_signupBtn">
+                            <div className="google-icon-wrapper">
+                              <img
+                                className="google-icon"
+                                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                                alt="google icon"
+                              />
+                            </div>
+                            <p className="btn-text">Sign up with google</p>
+                          </div>
+                          <div className="linkedin-btn common_signupBtn">
+                            <div className="google-icon-wrapper">
+                              <img
+                                className="google-icon"
+                                src="https://www.svgrepo.com/show/475661/linkedin-color.svg"
+                                loading="lazy"
+                                alt="google icon"
+                              />
+                            </div>
+                            <p className="btn-text">Sign up with LinkedIn</p>
+                          </div>
+                        </div>
                         <div class="separator">
                           <div class="line"></div>
                           <div class="or">or</div>
                           <div class="line"></div>
                         </div>
                         <Row>
-                          <Col>
+                          <Col className="mb-4">
                             <Form.Group controlId="firstName">
                               <Form.Label>First Name:</Form.Label>
                               <Form.Control
                                 type="text"
                                 value={firstName}
+                                placeholder="Enter you first name"
                                 onChange={(event) =>
                                   setFirstName(event.target.value)
                                 }
@@ -121,12 +133,13 @@ function Signup() {
                               </Form.Control.Feedback>
                             </Form.Group>
                           </Col>
-                          <Col>
+                          <Col className="mb-4">
                             <Form.Group controlId="lastName">
                               <Form.Label>Last Name:</Form.Label>
                               <Form.Control
                                 type="text"
                                 value={lastName}
+                                placeholder="Enter you last name"
                                 onChange={(event) =>
                                   setLastName(event.target.value)
                                 }
@@ -138,48 +151,65 @@ function Signup() {
                             </Form.Group>
                           </Col>
                         </Row>
-                        <Form.Group controlId="email">
+                        <Form.Group className="mb-4" controlId="email">
                           <Form.Label>Email:</Form.Label>
                           <Form.Control
                             type="email"
                             value={email}
+                            placeholder="Enter you best email"
                             onChange={(event) => setEmail(event.target.value)}
                             isInvalid={!!errors.email}
                           />
-                          <Form.Control.Feedback type="invalid   ">
+                          <Form.Control.Feedback type="invalid">
                             {errors.email}
                           </Form.Control.Feedback>
                         </Form.Group>
 
-                        <Form.Group controlId="password">
+                        <Form.Group className="mb-4" controlId="password">
                           <Form.Label>Password:</Form.Label>
-                          <div className="input-group">
+                          <div className="input-group password_group">
                             <Form.Control
                               type={showPassword ? "text" : "password"}
                               value={password}
+                              placeholder="Enter you password"
+                              className="password_input"
                               onChange={(event) =>
                                 setPassword(event.target.value)
                               }
+                              style={{
+                                borderRight: errors.password
+                                  ? "1px solid red"
+                                  : "none",
+                                borderRadius: errors.password ? "5px" : "0px",
+                              }}
                               isInvalid={!!errors.password}
-                              required
                             />
-                            <Button
-                              variant="outline-secondary"
-                              onClick={() => setShowPassword(!showPassword)}
-                            >
-                              {showPassword ? (
-                                <i className="bi bi-eye-slash"></i>
-                              ) : (
-                                <i className="bi bi-eye"></i>
-                              )}
-                            </Button>
+                           
+                              <Button
+                                className="eye_btn"
+                                variant="outline-secondary"
+                                style={{
+                                  color: "black",
+                                  boxShadow: "none",
+                                  backgroundColor: "transparent",
+                                }}
+                                onClick={() => setShowPassword(!showPassword)}
+                              >
+                                {showPassword ? (
+                                  <i className="bi bi-eye-slash"></i>
+                                ) : (
+                                  <i className="bi bi-eye"></i>
+                                )}
+                              </Button>
+                            
+
                             <Form.Control.Feedback type="invalid">
                               {errors.password}
                             </Form.Control.Feedback>
                           </div>
                         </Form.Group>
 
-                        <Form.Group controlId="country">
+                        <Form.Group className="mb-4" controlId="country">
                           <Form.Label>Country:</Form.Label>
                           <Form.Control
                             as="select"
@@ -202,10 +232,16 @@ function Signup() {
                           </Form.Control.Feedback>
                         </Form.Group>
 
-                        <Form.Group controlId="termsAgreed">
+                        <Form.Group className="mb-4" controlId="termsAgreed">
                           <Form.Check
                             type="checkbox"
-                            label="Yes, I understand and agree to the Jobify Terms of Service"
+                            label={
+                              <span>
+                                I have read and agree to the{" "}
+                                <a href="/terms-of-service">Terms of Service</a>
+                              </span>
+                            }
+                            className="terms_services"
                             checked={termsAgreed}
                             onChange={(event) =>
                               setTermsAgreed(event.target.checked)
@@ -217,7 +253,15 @@ function Signup() {
                           </Form.Control.Feedback>
                         </Form.Group>
 
-                        <Button type="submit">Submit</Button>
+                        <Button type="submit" className="commonBtn">
+                          Submit
+                        </Button>
+                        <div className="login_link d-flex align-items-center justify-content-center mt-3 pb-4">
+                          <p className="mb-0 me-2">Already have an account?</p>
+                          <Link className="class_Link" to="/login">
+                            Log in
+                          </Link>
+                        </div>
                       </Form>
                     </div>
                   </div>
